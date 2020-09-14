54899d548b0f65ca03dfe2667a736064bcd71422
Mr_Sandeep <46187079+sandeepmudari@users.noreply.github.com>
2020-09-14 19:35:39 +0530

    Add files via upload

---
+++ Notebook1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Week 1 Capstone Project (IBM Applied Data Science)
+# 
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+print("Hello Capstone Project Coursera!")
+
+
+# In[ ]:
+
+
+
+
